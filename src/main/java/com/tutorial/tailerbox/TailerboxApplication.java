a5636910c5cc42dfaede203002378ab533a83ae7
Cheolwon Lee <chlwn.lee@gmail.com>
2021-11-25 16:38:06 +0900

    tailerbox initialize

---
+++ src/main/java/com/tutorial/tailerbox/TailerboxApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.tailerbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailerboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TailerboxApplication.class, args);
+    }
+
+}
