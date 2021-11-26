0f6bce5b4ecf4e9efe51f6d8dfc975a5ac1bb069
Cheolwon Lee <chlwn.lee@gmail.com>
2021-11-26 18:39:56 +0900

    Added - UserController

--- src/main/java/com/tutorial/tailerbox/controller/UserController.java
+++ src/main/java/com/tutorial/tailerbox/controller/UserController.java
@@ -2,18 +2,27 @@ package com.tutorial.tailerbox.controller;
 
 import com.tutorial.tailerbox.data.entity.User;
 import com.tutorial.tailerbox.data.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @RestController
 public class UserController {
 
-    /*@GetMapping(value = "/users")
-    public List<User> getUsers() {
-        // Todo Implements ...
-    }*/
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(value = "/users")
+    public List<User> getUsers(@RequestParam Map<String, String> params) {
+        log.info("RequestParam : {}", params);
+        return userRepository.findAll();
+    }
 }
