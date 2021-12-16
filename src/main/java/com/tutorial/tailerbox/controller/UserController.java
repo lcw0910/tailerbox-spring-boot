256503c6047343be40e28ddbd57a40bfcd1791e9
Cheolwon Lee <chlwn.lee@gmail.com>
2021-12-16 21:42:43 +0900

    modified main

--- src/main/java/com/tutorial/tailerbox/controller/UserController.java
+++ src/main/java/com/tutorial/tailerbox/controller/UserController.java
@@ -2,7 +2,5 @@ package com.tutorial.tailerbox.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class UserController {}
